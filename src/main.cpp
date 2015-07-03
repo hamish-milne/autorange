4f8345ee121db4e0221e8534cdd40aa89354c043
hamish-milne <hamishmilne83@gmail.com>
2015-07-03 13:32:02 +0100

    Added unit test framework

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,6 @@
-
-#include "fixed_ops.hpp"
-
-#include <iostream>
-using std::cout;
-using std::endl;
-
-using namespace autorange;
+#include "UnitTest++.h"
 
 int main()
 {
-	fixed<-2, 2, P(0.000000001), fixed_policy, 512> a(0.333333333333333333);
-	fixed<-2, 2, P(0.000000001), fixed_policy, 512> b(-2.0);
-	int_type<0, 256> d(127);
-	auto c = autorange::invert(a);
-
-	cout.precision(12);
-	cout << a.precision << endl << c.to_double() << endl << c.error << endl << c.precision;
-
+	UnitTest::RunAllTests();
 }
---
+++ src/test/Basic.cpp
@@ -0,0 +1,33 @@
+#include "UnitTest++.h"
+
+#include "../fixed_type.hpp"
+
+#include <vector>
+#include <cstdint>
+#include <climits>
+
+using namespace autorange;
+using std::vector;
+
+namespace
+{
+
+
+	template<int64_t min, int64_t max>
+	void int_test()
+	{
+		static const vector<int64_t> numbers = {0, 1, -1, -100, 100};
+
+		for(size_t i = 0; i < numbers.size(); i++)
+		{
+			auto n = numbers[i];
+			if(n >= min && n <= max)
+				CHECK_EQUAL(n, (int)((int_type<min, max>(n)).to_double()));
+		}
+	}
+
+	TEST(MyFirstTest)
+	{
+		int_test<-100, 100>();
+	}
+}

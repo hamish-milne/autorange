54241faa44f231f0738a66884b8b406e4039ffca
hamish-milne <hamishmilne83@gmail.com>
2015-07-06 13:38:45 +0100

    Added fixed_const

--- src/fixed_const.hpp
+++ src/fixed_const.hpp
@@ -1,6 +1,20 @@
+#ifndef FIXED_CONST_HPP
+#define FIXED_CONST_HPP
 
+#include "helpers.hpp"
 
 namespace autorange
 {
+	static constexpr uint64_t D(double real)
+	{
+		return (ceil(log2(real)) & 0xFF) | (ceil(real * pow2(24 - (double)log2(real))) << 8);
+	}
 
+	template<uint64_t data, int precision = 0>
+	struct fixed_const
+	{
+		static constexpr double value = (data >> 8) * pow2((double)(int8_t)(data & 0xFF) - 24);
+	};
 }
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "UnitTest++.h"
 
 #include "fixed_ops.hpp"
+#include "fixed_const.hpp"
 using namespace autorange;
 
 #include <iostream>
@@ -12,9 +13,12 @@ int main()
 	fixed<0, 255, 8> a(0.125);
 	int_t<0, 2> b(2);
 
-	cout << b.min << endl << b.max << endl;
 	auto c = b/a;
 	cout << c.to_double() << endl;
+
+	const double real = 123.456;
+	fixed_const<D(123.456), 3> d;
+	cout << d.value << endl;
 
 	UnitTest::RunAllTests();
 }
